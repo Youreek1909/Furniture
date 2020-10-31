3b059f7fb39880aaa7854fa7049c7b71005052f7
Youreek Cenusa <youreek91@gmail.com>
2020-10-31 00:53:52 +0000

    few changes in js

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ $(document).ready( () => {
 });
 
 
-//// Variables ////
+//// Variables ///////////////////////////////////////
 let number = document.querySelector('.num').innerHTML;
 let num = document.querySelector('.num');
 let plus = document.querySelector('.plus');
@@ -45,23 +45,31 @@ const noLikes = document.querySelector('.no-likes');
 const searchIcon = document.querySelector('.search-icon');
 const searchForm = document.querySelector('.search__form');
 const searchInput = document.querySelector('.search__input');
-////
+const shoppingIcon = document.querySelector('.shopping-icon');
+const shoppingList = document.querySelector('.shopping__list');
+const shoppingLink = document.querySelector('.shopping__link');
+const deleteBtn = document.querySelector('.shopping__list--delete-btn');
+////////////////////////////////////////////////////////
 
+
+///// plus button
 plus.addEventListener('click', () => {
   number++;
   num.innerHTML = number;
 });
 
+///// minus button
 minus.addEventListener('click', () => {
   number > 1 ? number-- : '';
   num.innerHTML = number;
 });
 
-
+///// function toggle class
 const toggleClass = (el, cl) => {
   el.classList.contains(cl) ? el.classList.remove(cl) : el.classList.add(cl);
 }
 
+///// button save items
 saveBtn.addEventListener('click', () => {
   toggleClass(heartFull, 'hide');
   toggleClass(heart, 'hide');
@@ -69,14 +77,7 @@ saveBtn.addEventListener('click', () => {
   toggleClass(noLikes, 'hide');
 });
 
-
-cart.addEventListener('click', () => {
-  shoppingNum++;
-  shoppingItems.innerHTML = shoppingNum;
-  shoppingItems.style.display = 'block';
-});
-
-
+///// search icon show
 searchIcon.addEventListener('click', () => {
   toggleClass(searchForm, 'hide');
   toggleClass(searchForm, 'animated-search-form');
@@ -85,7 +86,54 @@ searchIcon.addEventListener('click', () => {
 });
 
 
-////Test search button click
+////Test search button click//////////////////////////////////
 const btnSubmit = () => {
   alert(searchInput.value);
 }
+
+////cart functionality ///////////////////////////////////////
+const cartItem = () => {
+  let markup = `
+  <a href="#" class="shopping__link">
+    <div class="shopping__list--items">
+      <div class="shopping__list--name">
+        <p>Adda parisian sofa</p>
+      </div>
+      <div class="shopping__list--picture">
+        <img src="img/img-6.jpg" alt="Picture">
+      </div>
+      <div class="shopping__list--delete">
+        <button class="shopping__list--delete-btn">
+          <i class="fas fa-times delete-item"></i>
+        </button>
+      </div>
+    </div>
+  </a> `;
+  shoppingList.insertAdjacentHTML('beforeend', markup);
+};
+
+///// add items to the cart
+cart.addEventListener('click', () => {
+  shoppingNum++;
+  shoppingItems.innerHTML = shoppingNum;
+  shoppingItems.style.display = 'block';
+  cartItem();
+  console.log(deleteBtn);
+});
+
+
+////open cart list only if there is some item
+shoppingIcon.addEventListener('click', () => {
+  if(shoppingNum >= 1) {
+    toggleClass(shoppingList, 'hide');
+  }
+});
+
+
+////// hide cart list on mouse leave
+shoppingList.addEventListener('mouseleave', () => {
+  toggleClass(shoppingList, 'hide');
+});
+
+
+//// delete item from the cart
